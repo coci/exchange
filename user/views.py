ba9c6baafa2ff2b70973f5476b9bdfc87f380f92
soroush <soroush.safari1992Gmailc.om>
2023-01-30 22:04:28 +0330

    add: permission for user endpoint

--- user/views.py
+++ user/views.py
@@ -1,7 +1,9 @@
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from permission.method_permission import method_permission_classes
 from user.serializers import RegisterUserSerializer, UserDetailSerializer, UpdateUserSerializer
 
 
@@ -17,10 +19,12 @@ class UserApi(APIView):
 		else:
 			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+	@method_permission_classes([IsAuthenticated])
 	def get(self, request):
 		serializer = UserDetailSerializer(instance=request.user)
 		return Response(serializer.data, status=status.HTTP_200_OK)
 
+	@method_permission_classes([IsAuthenticated])
 	def put(self, request):
 		serializer = UpdateUserSerializer(instance=request.user, data=request.data, many=False, partial=True)
 
