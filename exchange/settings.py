e8e9f022c9177778ca119134291ee57801b526b3
soroush <soroush.safari1992Gmailc.om>
2023-01-30 21:59:49 +0330

    add: django jwt dependency and authentication

--- exchange/settings.py
+++ exchange/settings.py
@@ -38,7 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+
+    'user.apps.UserConfig',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
