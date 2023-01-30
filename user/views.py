25a384fd830954bad736d03a5073eeddb9086975
soroush <soroush.safari1992Gmailc.om>
2023-01-30 21:45:20 +0330

    add : user update endpoint

--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from user.serializers import RegisterUserSerializer
+from user.serializers import RegisterUserSerializer, UserDetailSerializer, UpdateUserSerializer
 
 
 class UserApi(APIView):
@@ -13,6 +13,19 @@ class UserApi(APIView):
 
 			serializer.save()
 
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		else:
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	def get(self, request):
+		serializer = UserDetailSerializer(instance=request.user)
+		return Response(serializer.data, status=status.HTTP_200_OK)
+
+	def put(self, request):
+		serializer = UpdateUserSerializer(instance=request.user, data=request.data, many=False, partial=True)
+
+		if serializer.is_valid():
+			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		else:
 			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
