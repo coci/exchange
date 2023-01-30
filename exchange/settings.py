14c351f617788dfd5f5d1a7451ebb4573d084f89
soroush <soroush.safari1992Gmailc.om>
2023-01-30 21:48:07 +0330

    fix: template path

--- exchange/settings.py
+++ exchange/settings.py
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'exchange.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-	    ,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
