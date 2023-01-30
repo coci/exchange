93c099c7cc942a89064f93d6ad3e667a16d645ad
soroush <soroush.safari1992Gmailc.om>
2023-01-30 22:35:53 +0330

    fix: http status code

--- user/views.py
+++ user/views.py
@@ -30,6 +30,6 @@ class UserApi(APIView):
 
 		if serializer.is_valid():
 			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
+			return Response(serializer.data, status=status.HTTP_200_OK)
 		else:
 			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
