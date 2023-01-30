ebe00745ecf033707dab50c8a3e687ea363c87f4
soroush <soroush.safari1992Gmailc.om>
2023-01-30 21:26:28 +0330

    rename : user detail serializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,11 +1,10 @@
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
 
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserDetailSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = ["id", "first_name", "last_name", "username"]
