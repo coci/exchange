cce842536c59f16badfe787f5d153fa2c5833edb
soroush <soroush.safari1992Gmailc.om>
2023-01-31 18:10:02 +0330

    add order model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class OrderStatus(models.TextChoices):
+	success = 'success'
+	pending = 'pending'
+
+
+class Order(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	coin = models.CharField(max_length=50)
+	coin_amount = models.IntegerField()
+	order_price = models.IntegerField()
+	status = models.CharField(max_length=20, choices=OrderStatus.choices)
