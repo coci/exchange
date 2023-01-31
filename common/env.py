e2b606239710ace1fbc1a541313aa77c5388734e
soroush <soroush.safari1992Gmailc.om>
2023-01-31 23:18:53 +0330

    fix: read env from os

--- common/env.py
+++ common/env.py
@@ -1,7 +1,7 @@
 import os
 
 COIN_PRICE = int(os.getenv("COIN_PRICE", 4))
-ORDER_LIMIT = int(os.getenv("COIN_PRICE", 10))
+ORDER_LIMIT = int(os.getenv("ORDER_LIMIT", 10))
 REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
 REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 REDIS_DB = int(os.getenv('REDIS_DB', 0))
