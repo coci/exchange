a07070b9cb147df0e361bdef682223bf84376cb3
soroush <soroush.safari1992Gmailc.om>
2023-01-31 18:26:40 +0330

    fix : refactor order operation

--- order/operations.py
+++ order/operations.py
@@ -4,20 +4,28 @@ import redis
 from pickle import loads, dumps
 
 from django.db import transaction
+from django.db.models import Sum
 
 from common.env import REDIS_DB, REDIS_HOST, COIN_PRICE
 from order.models import Order, OrderStatus
 
 
 def buy_from_exchange(coin, amount):
-	pass
-
-
-def set_order_success_status(orders_id):
-	orders = Order.objects.filter(pk__in=orders_id).update(status=OrderStatus.success)
+	print(coin, amount)
 
 
 @transaction.atomic
+def check_pending_order(coin):
+	redis_connection = redis.Redis(host=REDIS_HOST, db=REDIS_DB)
+	pending_order, total_sum = loads(redis_connection.get(coin))
+
+	if total_sum >= 10:
+		orders = Order.objects.filter(pk__in=pending_order.keys())
+		buy_from_exchange(coin, orders.aggregate((Sum('coin_amount')['coin_amount__sum'])))
+		orders.update(status=OrderStatus.success)
+		redis_connection.delete(coin)
+
+
 def set_pending_order(order_id, coin, amount):
 	redis_connection = redis.Redis(host=REDIS_HOST, db=REDIS_DB)
 	total_price = amount * COIN_PRICE
@@ -25,8 +33,14 @@ def set_pending_order(order_id, coin, amount):
 	if redis_connection.hgetall(coin):
 		pending_order, total_sum = loads(redis_connection.get(coin))
 
-		if total_price + total_sum >= 10:
-			pass
+		pending_order[order_id] = total_price
+		total_sum += total_price
+
+		data = dumps([{str(order_id): total_price}, total_price])
+		redis_connection.set(coin, data)
+
+		check_pending_order(coin=coin)
+
 	else:
 		data = dumps([{str(order_id): total_price}, total_price])
 		redis_connection.set(coin, data)
