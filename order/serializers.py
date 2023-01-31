086f21c26ac117e50d7a6ad2d7bb293e07483c4c
soroush <soroush.safari1992Gmailc.om>
2023-01-31 18:10:17 +0330

    add order serializer

---
+++ order/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+from order.models import Order
+
+
+class OrderInputSerializer(serializers.Serializer):
+	coin = serializers.CharField(required=True)
+	amount = serializers.IntegerField(required=True)
+
+	def validate(self, attrs):
+		if attrs['amount'] <= 0:
+			raise serializers.ValidationError(
+				{
+					"amount": "Must be positive and greater than zero."
+				}
+			)
+		return attrs
+
+
+class OrderSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Order
+		filter = "__all__"
