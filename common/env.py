1362a85b5045f2d2a8f6c4feefe113786e75640e
soroush <soroush.safari1992Gmailc.om>
2023-01-31 21:50:35 +0330

    add: redis port

--- common/env.py
+++ common/env.py
@@ -3,4 +3,5 @@ import os
 COIN_PRICE = int(os.getenv("COIN_PRICE", 4))
 ORDER_LIMIT = int(os.getenv("COIN_PRICE", 10))
 REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
+REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 REDIS_DB = int(os.getenv('REDIS_DB', 0))
