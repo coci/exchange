c05591d4a7ec20d3536710112c05c94f0bebd0be
soroush <soroush.safari1992Gmailc.om>
2023-01-31 19:03:00 +0330

    fix: REDIS_HOST

--- common/env.py
+++ common/env.py
@@ -2,5 +2,5 @@ import os
 
 COIN_PRICE = int(os.getenv("COIN_PRICE", 4))
 ORDER_LIMIT = int(os.getenv("COIN_PRICE", 10))
-REDIS_HOST = int(os.getenv('REDIS_HOST', 'localhost'))
+REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
 REDIS_DB = int(os.getenv('REDIS_DB', 0))
