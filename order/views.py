bfd3ac7a7b60f614cde05eb9139c11105e0295f9
soroush <soroush.safari1992Gmailc.om>
2023-01-31 18:10:41 +0330

    add order endpoint

--- order/views.py
+++ order/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.db import transaction
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from common.env import COIN_PRICE, ORDER_LIMIT
+from order.models import Order, OrderStatus
+from order.operations import buy_from_exchange, set_pending_order
+from order.serializers import OrderInputSerializer, OrderSerializer
+from user_balance.models import UserBalance
+
+
+class Payment(APIView):
+	permission_classes = [IsAuthenticated, ]
+
+	@transaction.atomic
+	def post(self, request):
+		serializer = OrderInputSerializer(data=request.data)
+
+		if serializer.is_valid():
+			coin = serializer.validated_data['coin']
+			amount = serializer.validated_data['amount']
+
+		else:
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+		user_balance = UserBalance.objects.get(user=request.user)
+
+		total_price = amount * COIN_PRICE
+
+		if not user_balance.balance >= total_price:
+			response = {
+				"status": "error",
+				"message": "you don't have credit."
+			}
+			return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+		# minus user balance
+		user_balance.balance -= total_price
+		user_balance.save()
+
+		if total_price >= ORDER_LIMIT:
+			buy_from_exchange(coin=coin, amount=amount)
+
+			order = Order.objects.create(
+				user=request.user,
+				coin=coin,
+				coin_amount=amount,
+				order_price=total_price,
+				status=OrderStatus.success
+			)
+
+		else:
+			order = Order.objects.create(
+				user=request.user,
+				coin=coin,
+				coin_amount=amount,
+				order_price=total_price,
+				status=OrderStatus.pending
+			)
+
+			set_pending_order(order_id=order.pk, coin=coin, amount=amount)
+
+		serializer = OrderSerializer(instance=order, many=False)
+		return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
