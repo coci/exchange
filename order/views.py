57a032a03d799e5ebf78959888ded91457e17810
soroush <soroush.safari1992Gmailc.om>
2023-01-31 19:05:25 +0330

    fix: Http code response

--- order/views.py
+++ order/views.py
@@ -63,4 +63,4 @@ class Payment(APIView):
 			set_pending_order(order_id=order.pk, coin=coin, amount=amount)
 
 		serializer = OrderSerializer(instance=order, many=False)
-		return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+		return Response(serializer.data, status=status.HTTP_200_OK)
